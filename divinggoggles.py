6f55f4672d21954ee5addc652ee154d4cbcc9430
Matthias Stahl <matthias.stahl87@web.de>
2018-09-13 17:36:25 +0200

    added some database logic for tests

--- divinggoggles.py
+++ divinggoggles.py
@@ -6,10 +6,52 @@
 # Matthias Stahl, 2018
 
 import sys
+import sqlite3
+from sqlite3 import Error
+import networkx as nx
 
-default_database_file = "target_psmlookup.sqlite"
+default_database_file = "target_psmlookup.sql"
 
-def buildJSONLookup(database_file):
+def _create_connection(database_file):
+    """ creates a database connection to the SQLite database
+        specified by the db_file
+    :param database_file: database file
+    :return: connection object or None
+    """
+    try:
+        conn = sqlite3.connect(database_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+
+def buildJSONLookup(database_file = default_database_file):
+    """ builds the JSON lookup and handles database connection
+    :param database_file: database file
+    :return: bool if built was successful
+    """
+    
+    # generate new graph
+    G = nx.Graph()
+    
+    # connect to database and fetch relevant linkages
+    conn = _create_connection(database_file = database_file)
+    cur = conn.cursor()
+    
+    # first go for the psm to peptide linkage
+    query = "SELECT psms.psm_id, peptide_sequences.sequence FROM psms INNER JOIN peptide_sequences ON psms.pep_id = peptide_sequences.pep_id"
+    cur.execute(query)
+    peptide_to_psms = cur.fetchall()
+    
+    # next go for the peptides to proteins association
+    query = "SELECT peptide_sequences.sequence, protein_psm.protein_acc FROM protein_psm INNER JOIN psms ON protein_psm.psm_id = psms.psm_id INNER JOIN peptide_sequences ON psms.pep_id = peptide_sequences.pep_id"
+    cur.execute(query)
+    peptides_to_proteins = cur.fetchall()
+    
+    
+    conn.close()
     return(True)
 
 if __name__ == "__main__":
