c399cc5381af6e3c3bb3046a5c33384fed6cb281
Matthias Stahl <matthias.stahl87@web.de>
2018-09-13 20:49:57 +0200

    finished database requests

--- divinggoggles.py
+++ divinggoggles.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Creates a JSON based lookup table linking protein groups to proteins to peptides to PSms.
+# Creates a graph-based lookup table linking protein groups to proteins to peptides to PSms.
 #
 #
 # Matthias Stahl, 2018
@@ -27,7 +27,7 @@ def _create_connection(database_file):
     return None
 
 
-def buildJSONLookup(database_file = default_database_file):
+def buildInferenceNetwork(database_file = default_database_file):
     """ builds the JSON lookup and handles database connection
     :param database_file: database file
     :return: bool if built was successful
@@ -41,15 +41,30 @@ def buildJSONLookup(database_file = default_database_file):
     cur = conn.cursor()
     
     # first go for the psm to peptide linkage
-    query = "SELECT psms.psm_id, peptide_sequences.sequence FROM psms INNER JOIN peptide_sequences ON psms.pep_id = peptide_sequences.pep_id"
+    query = "SELECT psms.psm_id, peptide_sequences.sequence " \
+            "FROM psms " \
+            "INNER JOIN peptide_sequences " \
+            "ON psms.pep_id = peptide_sequences.pep_id"
     cur.execute(query)
-    peptide_to_psms = cur.fetchall()
+    psms_to_peptide = cur.fetchall()
     
     # next go for the peptides to proteins association
-    query = "SELECT peptide_sequences.sequence, protein_psm.protein_acc FROM protein_psm INNER JOIN psms ON protein_psm.psm_id = psms.psm_id INNER JOIN peptide_sequences ON psms.pep_id = peptide_sequences.pep_id"
+    query = "SELECT peptide_sequences.sequence, protein_psm.protein_acc " \
+            "FROM protein_psm " \
+            "INNER JOIN psms " \
+            "ON protein_psm.psm_id = psms.psm_id " \
+            "INNER JOIN peptide_sequences " \
+            "ON psms.pep_id = peptide_sequences.pep_id"
     cur.execute(query)
     peptides_to_proteins = cur.fetchall()
     
+    # finally the protein to proteon group linkage
+    query = "SELECT protein_group_content.protein_acc, protein_group_master.protein_acc " \
+            "FROM protein_group_content " \
+            "INNER JOIN protein_group_master " \
+            "ON protein_group_content.master_id = protein_group_master.master_id"
+    cur.execute(query)
+    proteins_to_protein_group = cur.fetchall()
     
     conn.close()
     return(True)
@@ -62,8 +77,8 @@ if __name__ == "__main__":
         print("No database file given. Will use " + default_database_file + " file.")
         database_file = default_database_file
     
-    # build JSON lookup
-    if (buildJSONLookup(database_file)):
+    # build network
+    if (buildInferenceNetwork(database_file)):
         print("JSON lookup was successfully built.")
     else:
         print("An error occurred during JSON lookup construction.")
