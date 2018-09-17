efa2a7606c639ecaf2ea09b98e738f60f69ed09a
Matthias Stahl <matthias.stahl87@web.de>
2018-09-18 00:13:53 +0200

    initial commit: peptides2proteins, creates peptide to protein lookup json

---
+++ peptides2proteins.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# Creates a graph-based lookup linking protein groups to proteins to peptides to PSms.
+#
+#
+# Matthias Stahl, 2018
+
+import sys
+import sqlite3
+from sqlite3 import Error
+import networkx as nx
+from networkx.readwrite import json_graph
+import json
+
+database_file = "target_psmlookup.sql"
+file_name = "graph.json"
+
+def _create_connection(database_file):
+    try:
+        conn = sqlite3.connect(database_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def _fetchPeptides2Proteins(conn):
+    cur = conn.cursor()
+    query = "SELECT peptide_sequences.sequence, protein_psm.protein_acc " \
+            "FROM protein_psm " \
+            "INNER JOIN psms " \
+            "ON protein_psm.psm_id = psms.psm_id " \
+            "INNER JOIN peptide_sequences " \
+            "ON psms.pep_id = peptide_sequences.pep_id"
+    cur.execute(query)
+    return(cur.fetchall())
+
+def buildLookup(database_file = database_file):
+    # connect to database and fetch relevant linkages
+    conn = _create_connection(database_file = database_file)
+    
+    # fetch data and gradually build graph
+    lookup = {}
+    for peptide, protein in _fetchPeptides2Proteins(conn):
+        if (protein in lookup.keys()):
+            lookup[protein].append(peptide)
+        else:
+            lookup[protein] = [peptide]
+    
+    # close database connection
+    conn.close()
+    
+    return(lookup)
+
+if __name__ == "__main__":
+    # look for command line arguments
+    if (len(sys.argv) > 1):
+        database_file = sys.argv[1]
+    if (len(sys.argv) > 2):
+        file_name = sys.argv[2]
+    
+    # build graph
+    lookup = buildLookup(database_file)
+    
+    # save
+    with open(file_name, 'w') as f:
+        json.dump(lookup, f)
+    
