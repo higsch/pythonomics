4daf4ca88dba622959efd9653a58ddbc4af85fb5
Matthias Stahl <matthias.stahl87@web.de>
2018-09-22 02:14:13 +0200

    changed to d3 format

--- peptides2proteins.py
+++ peptides2proteins.py
@@ -12,7 +12,7 @@ import json
 import gzip
 
 database_file = "target_psmlookup.sql"
-file_name = "graph.json"
+file_name = "inference.json"
 
 def _create_connection(database_file):
     try:
@@ -25,12 +25,13 @@ def _create_connection(database_file):
 
 def _fetchPeptides2Proteins(conn):
     cur = conn.cursor()
-    query = "SELECT peptide_sequences.sequence, protein_psm.protein_acc " \
+    query = "SELECT protein_psm.protein_acc, peptide_sequences.sequence " \
             "FROM protein_psm " \
             "INNER JOIN psms " \
             "ON protein_psm.psm_id = psms.psm_id " \
             "INNER JOIN peptide_sequences " \
-            "ON psms.pep_id = peptide_sequences.pep_id"
+            "ON psms.pep_id = peptide_sequences.pep_id " \
+            "ORDER BY protein_psm.protein_acc"
     for row in cur.execute(query):
         yield row
 
@@ -40,7 +41,7 @@ def buildLookup(database_file = database_file):
     
     # fetch data and gradually build graph
     lookup = {}
-    for peptide, protein in _fetchPeptides2Proteins(conn):
+    for protein, peptide in _fetchPeptides2Proteins(conn):
         if (protein in lookup.keys()):
             if (peptide in lookup[protein]):
                 continue
@@ -53,6 +54,18 @@ def buildLookup(database_file = database_file):
     
     return(lookup)
 
+def buildD3Json(lookup):
+    links = []
+    nodes = []
+    nodes.extend([[*lookup], lookup.values()])
+    nodes = set(nodes)
+    
+    nodes[0]
+    for protein, peptides in lookup.items():
+        links.append({"source": nodes.index(protein), "target": nodes.index(peptide)})
+        
+    return({"nodes": nodes, "links": links})
+
 if __name__ == "__main__":
     # look for command line arguments
     if (len(sys.argv) > 1):
@@ -62,6 +75,7 @@ if __name__ == "__main__":
     
     # build graph
     lookup = buildLookup(database_file)
+    d3_json = buildD3Json(lookup)
     
     # save
     with open(file_name, 'w') as f:
